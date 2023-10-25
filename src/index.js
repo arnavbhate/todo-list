e418bdaf757e87448823073656c617b9ccb519a0
Arnav Bhate <bhatearnav@gmail.com>
2023-10-26 01:19:18 +0530

    Modified default files for use
    Changed icons and css. Removed some default text. Removed some other
    files. Added a package. Added a license file.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { FluentProvider, webLightTheme } from '@fluentui/react-components';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <FluentProvider theme={webLightTheme}>
+      <App />
+    </FluentProvider>
   </React.StrictMode>
 );
 
